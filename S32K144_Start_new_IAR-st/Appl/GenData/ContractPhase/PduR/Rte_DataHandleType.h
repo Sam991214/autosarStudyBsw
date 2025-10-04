1d0ed6b0a0b75bf429d5e6f149120ef9393a43e5
Tempura <3335797057@qq.com>
2025-10-05 05:04:25 +0800

    1.提交基础版autosar

---
+++ S32K144_Start_new_IAR-st/Appl/GenData/ContractPhase/PduR/Rte_DataHandleType.h
@@ -0,0 +1,27 @@
+/**********************************************************************************************************************
+ *  COPYRIGHT
+ *  -------------------------------------------------------------------------------------------------------------------
+ *
+ *                This software is copyright protected and proprietary to Vector Informatik GmbH.
+ *                Vector Informatik GmbH grants to you only those rights as set out in the license conditions.
+ *                All other rights remain with Vector Informatik GmbH.
+ *  -------------------------------------------------------------------------------------------------------------------
+ *  FILE DESCRIPTION
+ *  -------------------------------------------------------------------------------------------------------------------
+ *          File:  Rte_DataHandleType.h
+ *        Config:  D:\WebDown\S32K144\S32K144 \x{2026}\S32K144 AUTOSAR\x{5DE5}\x{5177}\x{5305}\share_20230823\AUTOSAR_TOOLs\S32K144_SIP\MICROSAR\CBD1800257_D01_S32K1xx\Applications\S32K144_Start_new_IAR/"D:/WebDown/S32K144/S32K144 \x{2026}/S32K144 AUTOSAR\x{5DE5}\x{5177}\x{5305}/share_20230823/AUTOSAR_TOOLs/S32K144_SIP/MICROSAR/CBD1800257_D01_S32K1xx/Applications/S32K144_Start_new_IAR/S32K144_Start.dpa"
+ *     SW-C Type:  PduR
+ *  Generated at:  Mon Aug 25 08:01:58 2025
+ *
+ *     Generator:  MICROSAR RTE Generator Version 4.19.0
+ *                 RTE Core Version 1.19.0
+ *       License:  CBD1800257
+ *
+ *   Description:  Header file containing Data Handle type declarations for component data structures (ContractPhase)
+ *********************************************************************************************************************/
+
+/* double include prevention */
+#ifndef _RTE_DATA_HANDLE_TYPE_H
+# define _RTE_DATA_HANDLE_TYPE_H
+
+#endif /* _RTE_DATA_HANDLE_TYPE_H */
---
+++ S32K144_Start_new_IAR-st/Appl/GenData/ContractPhase/PduR/SchM_PduR_Type.h
@@ -0,0 +1,35 @@
+/**********************************************************************************************************************
+ *  COPYRIGHT
+ *  -------------------------------------------------------------------------------------------------------------------
+ *
+ *                This software is copyright protected and proprietary to Vector Informatik GmbH.
+ *                Vector Informatik GmbH grants to you only those rights as set out in the license conditions.
+ *                All other rights remain with Vector Informatik GmbH.
+ *  -------------------------------------------------------------------------------------------------------------------
+ *  FILE DESCRIPTION
+ *  -------------------------------------------------------------------------------------------------------------------
+ *          File:  SchM_PduR_Type.h
+ *        Config:  D:\WebDown\S32K144\S32K144 \x{2026}\S32K144 AUTOSAR\x{5DE5}\x{5177}\x{5305}\share_20230823\AUTOSAR_TOOLs\S32K144_SIP\MICROSAR\CBD1800257_D01_S32K1xx\Applications\S32K144_Start_new_IAR/"D:/WebDown/S32K144/S32K144 \x{2026}/S32K144 AUTOSAR\x{5DE5}\x{5177}\x{5305}/share_20230823/AUTOSAR_TOOLs/S32K144_SIP/MICROSAR/CBD1800257_D01_S32K1xx/Applications/S32K144_Start_new_IAR/S32K144_Start.dpa"
+ *    BSW Module:  PduR
+ *  Generated at:  Mon Aug 25 08:01:58 2025
+ *
+ *     Generator:  MICROSAR RTE Generator Version 4.19.0
+ *                 RTE Core Version 1.19.0
+ *       License:  CBD1800257
+ *
+ *   Description:  Module Interlink Types Header for BSW Module <PduR> (Contract Phase)
+ *********************************************************************************************************************/
+#ifndef SCHM_PDUR_TYPE_H
+# define SCHM_PDUR_TYPE_H
+
+# ifdef __cplusplus
+extern "C" {
+# endif  /* __cplusplus */
+
+# include "Rte_Type.h"
+
+# ifdef __cplusplus
+} /* extern "C" */
+# endif  /* __cplusplus */
+
+#endif /* SCHM_PDUR_TYPE_H */
